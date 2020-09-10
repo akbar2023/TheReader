50b9bb09db4a82697325d4674d6c8064360081bc
KHAN Akbar <akhan2023@yahoo.com>
2020-09-10 19:37:19 +0200

    wip

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {BookFormComponent} from './components/book-form/book-form.component';
-import {BookListComponent} from './components/book-list/book-list.component';
-import {MaterialModule} from '../material/material.module';
-
-
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MaterialModule
-  ],
-  declarations: [
-    BookFormComponent,
-    BookListComponent,
-  ],
-  exports: [
-    BookFormComponent,
-    BookListComponent,
-  ]
+  imports: [CommonModule, MaterialModule],
+  declarations: [BookFormComponent, BookListComponent],
+  exports: [BookFormComponent, BookListComponent],
 })
-export class BooksModule { }
+export class BooksModule {}
