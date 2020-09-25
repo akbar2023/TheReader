dbf00f966f0b7ae80ee04f7aa0a57e879ba42319
KHAN Akbar <akhan2023@yahoo.com>
2020-09-25 15:24:41 +0200

    clean

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,13 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MenuComponent} from './components/menu/menu.component';
-import {MaterialModule} from '../material/material.module';
-import {MatButtonModule} from '@angular/material/button';
-import {RouterModule} from '@angular/router';
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+import { MaterialModule } from '../material/material.module';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MaterialModule,
-    MatButtonModule,
-    RouterModule
-  ],
-  declarations: [
-    MenuComponent
-  ],
-  exports: [
-    MenuComponent
-  ]
+  imports: [CommonModule, MaterialModule, MatButtonModule, RouterModule],
+  declarations: [MenuComponent],
+  exports: [MenuComponent],
 })
-export class SharedModule {
-}
+export class SharedModule {}
