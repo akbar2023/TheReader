120c9e30c63a1c2adcf7d1649cbc2d9efad9a535
KHAN Akbar <akhan2023@yahoo.com>
2020-12-25 00:11:30 +0100

    footer ok

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
 import { MaterialModule } from '../material/material.module';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, RouterModule],
-  declarations: [MenuComponent],
-  exports: [MenuComponent],
+  declarations: [MenuComponent, FooterComponent],
+  exports: [MenuComponent, FooterComponent],
 })
 export class SharedModule {}
